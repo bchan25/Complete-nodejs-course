e2f761f46cdeb624ef110e278471564de2c14287
Ben Chan <ben@chanhq.com>
2020-03-25 19:55:28 +0000

    Challenge to use AppendFileSync using the documentation

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,3 +1,11 @@
 const fs = require('fs');
 
 fs.writeFileSync("notes.txt", "This file was created by Node.js");
+
+try {
+    fs.appendFileSync('notes.txt', 'Hello Ben');
+    console.log('The "data to append" was appended to file!');
+} catch (err) {
+/* Handle the error */
+    console.log(err);
+}
