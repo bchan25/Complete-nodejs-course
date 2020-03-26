1797111c05f95e6a53b86df0e533d747590bc9cd
Ben Chan <ben@chanhq.com>
2020-03-26 11:17:41 +0000

    Demo of writing a JSON file

--- playground/1-json.js
+++ playground/1-json.js
@@ -1,8 +1,14 @@
+const fs = require('fs');
+
 const book = {
     title: '4 Hour Wookweek',
     author: 'Tim Ferris'
 }
 
+const bookJSON = JSON.stringify(book);
+fs.writeFileSync('1-json.json', bookJSON);
+
+/* 
 // Convert Object to JSON
 const bookJSON = JSON.stringify(book);
 console.log(bookJSON);
@@ -10,3 +16,4 @@ console.log(bookJSON);
 // Parse JSON to use Data
 const parsedData = JSON.parse(bookJSON)
 console.log(parsedData.author)
+*/
