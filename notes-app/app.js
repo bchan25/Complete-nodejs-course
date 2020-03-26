44bb2f155bd6c511c1d6bbf774c1796279358926
Ben Chan <ben@chanhq.com>
2020-03-26 10:05:43 +0000

    Basic stub code added. $node app.js add

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,10 +1,13 @@
 const chalk = require('chalk');
 const getNotes = require('./notes');
 
-const msg = getNotes();
-console.log(msg);
+const command = process.argv[2];
 
-console.log(chalk.black.bgGreen('Success'));
+if(command === "add"){
+    console.log("Adding note!");
+} else if (command === "remove"){
+    console.log("Removing Note")
+}
 
 
-console.log(process.argv[2]);
+// console.log(process.argv[2]);
