8f016bf61097d21525570b78b4da7c92d07c2322
Ben Chan <ben@chanhq.com>
2020-03-26 16:24:32 +0000

    Basic Asynchronous example

---
+++ weather-app/app.js
@@ -0,0 +1,8 @@
+console.log('Starting')
+
+// Asychnronous Example: When something runs in background and program continues
+setTimeout(() => {
+    console.log('2 Second Timer')
+}, 2000)
+
+console.log('Stopping')
