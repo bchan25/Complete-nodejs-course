b4c4ee8d58a4bcd94dbf27eb261b0c9cb8433c15
Ben Chan <ben@chanhq.com>
2020-03-27 09:33:30 +0000

    Made a simple request to darksky api

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,1 +1,8 @@
+const request = require('postman-request');
+
 const url = 'https://api.darksky.net/forecast/04a3e3e70bbf052c3c67e70b1e9a19bd/37.8267,-122.4233'
+
+request({url}, function (error, response, body) {
+    const data = JSON.parse(response.body)
+    console.log(data.currently)
+ });
