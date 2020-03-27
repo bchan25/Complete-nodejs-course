644235922438c3fc7410313de13a1cae6f7671cb
Ben Chan <ben@chanhq.com>
2020-03-27 09:45:03 +0000

    Generate a custom forecast using api values

--- weather-app/app.js
+++ weather-app/app.js
@@ -3,5 +3,7 @@ const request = require('postman-request');
 const url = 'https://api.darksky.net/forecast/04a3e3e70bbf052c3c67e70b1e9a19bd/37.8267,-122.4233'
 
 request({url: url, json: true}, function (error, response, body) {
-    console.log(response.body.currently)
+    //console.log(response.body.currently)
+
+    console.log("It is currently " + response.body.currently.temperature + " degrees out. There is " + response.body.currently.precipProbability + "% chance of rain.") 
  });
