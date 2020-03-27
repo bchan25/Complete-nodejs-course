8d3605788303b7960a0dc8c48539a22a776296a9
Ben Chan <ben@chanhq.com>
2020-03-27 14:44:24 +0000

    Callback chaining

--- weather-app/app.js
+++ weather-app/app.js
@@ -2,12 +2,22 @@ const geocode = require('./utils/geocode');
 const forecast = require('./utils/forecast')
 
 
- geocode('London', (error, data) => {
-    console.log(data)
+ geocode('Flint', (error, data) => {
+    
+    if(error){
+        return console.log(error)
+    }
+    
+    forecast(data.latitude, data.longitude, (error, forecastData) => {
+        
+        if(error){
+            return console.log(error)
+        }
+        
+        console.log(data.location)
+        console.log(forecastData)
+    })
+
  })
 
 
-forecast(51.50722, -0.1275, (error, data) => {
-    console.log('Error', error)
-    console.log('Data', data)
-})
