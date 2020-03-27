eb5497ebe3f8859e1e601859d15995a0c711135a
Ben Chan <ben@chanhq.com>
2020-03-27 10:28:36 +0000

    Error url handle

--- weather-app/app.js
+++ weather-app/app.js
@@ -6,6 +6,8 @@ request({url: url, json: true}, function (error, response, body) {
 
     if(error){
         console.log('Unable to connect to weather service');
+    } else if(response.body.error){
+        console.log('Unable to find location')
     } else{
         console.log(response.body.daily.data[0].summary + " It is currently " + response.body.currently.temperature + " degrees out. There is " + response.body.currently.precipProbability + "% chance of rain.") 
     }
