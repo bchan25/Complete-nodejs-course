ae57628ebcfdad6f14f4b11feddc0cebef87d312
Ben Chan <ben@chanhq.com>
2020-03-29 10:51:01 +0100

    Replaced static pages with dynamic pages

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -5,9 +5,31 @@ const express = require('express')
 const app = express()
 const publicDirectory = path.join(__dirname, '../public')
 
+// Dynamic Templates
+app.set('view engine', 'hbs')
+// Static HTML Files
 app.use(express.static(publicDirectory))
 
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather',
+        name: 'Ben Chan'
+    })
+})
 
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: "About Me",
+        name: 'Ben Chan'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Help Me',
+        message: 'Enter your location and we will return the weather of your location.'
+    })
+})
 
 // app.com/weather
 app.get('/weather', (req, res) => {
